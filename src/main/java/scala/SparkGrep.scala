31fe190fb47e8f25e9840e5366d9400d11fa88e6
Arnabh Chakravarty <carn76@gmail.com>
2016-10-01 12:46:49 -0400

    Resolved all spark utilities

--- src/main/java/scala/SparkGrep.scala
+++ src/main/java/scala/SparkGrep.scala
@@ -3,14 +3,12 @@ package scala
 object SparkGrep {
 	def main(args: Array[String]) {
 
-
 		if (args.length < 3) {
 			System.err.println("Usage: SparkGrep <host> <input_file> <match_term>")
 			System.exit(1)
 		}
 
-		//SparkUtilities.countWords(args)
-
+		SparkUtilities.countWords(args)
 
 	}
 
