cbb893b1cfc5935445c479859d997a275a29646d
Arnabh Chakravarty <carn76@gmail.com>
2016-10-01 13:28:29 -0400

    Refactorted - removed java folder
    Scala Version 2.10.1
    Java Version 1.8_102

--- src/main/test/scala/SparkGrepTest.scala
+++ src/main/test/scala/SparkGrepTest.scala
@@ -1,12 +1,9 @@
-//import org.scalatest.FunSuite
-//
-///**
-// * Created by saur6410 on 9/24/16.
-// */
-//class SparkGrepTest extends FunSuite {
-//
-//  test("Run the test") {
-//    SparkGrep.main(Array("local[*]", "src/main/scala/SparkGrep.scala", "val"))
-//  }
-//
-//}
+import org.scalatest.FunSuite
+
+class SparkGrepTest extends FunSuite {
+
+  test("Run the test") {
+    SparkGrep.main(Array("local[*]", "src/main/java/scala/SparkGrep.scala", "val"))
+  }
+
+}
