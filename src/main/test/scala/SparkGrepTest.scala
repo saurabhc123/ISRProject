1129c4ec82c10a9d8b60e742b18855a4d8fac78c
Arnabh Chakravarty <carn76@gmail.com>
2016-10-01 13:34:25 -0400

    Second argument in Array corrected

--- src/main/test/scala/SparkGrepTest.scala
+++ src/main/test/scala/SparkGrepTest.scala
@@ -3,7 +3,7 @@ import org.scalatest.FunSuite
 class SparkGrepTest extends FunSuite {
 
   test("Run the test") {
-    SparkGrep.main(Array("local[*]", "src/main/java/scala/SparkGrep.scala", "val"))
+    SparkGrep.main(Array("local[*]", "src/main/scala/SparkGrep.scala", "val"))
   }
 
 }
