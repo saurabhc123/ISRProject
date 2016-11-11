7957cf5d9632510ee128d602a043d90469989071
Eric Williamson <ericrw96@vt.edu>
2016-11-10 20:22:05 -0500

    Adding labels, hardcoded for now

--- src/main/scala/DataRetriever.scala
+++ src/main/scala/DataRetriever.scala
@@ -40,29 +40,16 @@ import org.apache.hadoop.hbase.util.Base64
   */
 case class Tweet(id: String, tweetText: String, label: Option[Double] = None)
 object DataRetriever {
-  def convertScanToString(scan: Scan): String = {
-    val proto = ProtobufUtil.toScan(scan)
-    Base64.encodeBytes(proto.toByteArray)
-  }
   var _tableName: String = "ideal-cs5604f16" /*"ideal-cs5604f16-fake"*/
   var _colFam : String = "tweet"
   var _col : String = "cleantext" /*"text"*/
+
+
   def retrieveTweets(collectionID: String, sc : SparkContext): RDD[Tweet] = {
-    //val sc = new SparkContext(/*conf*/)
-    /* val hbaseConfig = HBaseConfiguration.create()
-    hbaseConfig.set(TableInputFormat.INPUT_TABLE, _tableName)
-    val scan = new Scan(Bytes.toBytes(collectionID), Bytes.toBytes(collectionID + '0'))
-    hbaseConfig.set(TableInputFormat.SCAN, convertScanToString(scan))
-    val hbaseRdd = sc.newAPIHadoopRDD(hbaseConfig,classOf[TableInputFormat],
-      classOf[org.apache.hadoop.hbase.io.ImmutableBytesWritable],
-      classOf[org.apache.hadoop.hbase.client.Result] )
-    hbaseRdd.map((x)=> rowToTweetConverter(x._2))
-    */val interactor = new HBaseInteraction(_tableName)
-        println("MAKING INTERACTOR")
-	    val result  = interactor.getRowsBetweenPrefix(collectionID, _colFam, _col)
-	        //result.foreach(println)
-		    //println("printed")
-		       sc.parallelize(result.iterator().map(r => rowToTweetConverter(r)).toList,30)
+    val interactor = new HBaseInteraction(_tableName)
+    println("MAKING INTERACTOR")
+    val result  = interactor.getRowsBetweenPrefix(collectionID, _colFam, _col)
+    sc.parallelize(result.iterator().map(r => rowToTweetConverter(r)).toList)
   }
 
   def rowToTweetConverter(result : Result): Tweet ={
--- src/main/scala/DataWriter.scala
+++ src/main/scala/DataWriter.scala
@@ -1,10 +1,5 @@
 package isr.project
-import isr.project.{HBaseInteraction, Tweet}
 import org.apache.spark.rdd.RDD
-import org.apache.hadoop.hbase.util.Bytes
-
-import scala.collection.JavaConverters._
-import scala.collection.mutable.ArrayBuffer
 /**
   * Created by Eric on 11/9/2016.
   */
@@ -14,20 +9,20 @@ object DataWriter {
     val _tableName: String = "cla-test-table"
     val _colFam : String = "cla-col-fam"
     val _col : String = "classification"
-    //tweets.foreach(println)
     val interactor = new HBaseInteraction(_tableName)
     tweets.collect.foreach(tweet => writeTweetToDatabase(tweet,interactor, _colFam, _col))
-    /*tweets.foreachPartition((tweets) => {
-      val interactor = new HBaseInteraction(_tableName)
-      println("MAKING INTERACTOR for table: " + _tableName)
-      tweets.foreach(tweet => writeTweetToDatabase(tweet,interactor, _colFam, _col))
-    })*/
-    println("Wrote to database " + tweets.count() + " tweets") 
-    //tweets.foreach(println) 
+    println("Wrote to database " + tweets.count() + " tweets")
  }
   def writeTweetToDatabase(tweet : Tweet, interactor: HBaseInteraction, colFam: String, col: String): Unit ={
-//	  println(Bytes.toBytes(tweet.label.getOrElse(99.0).toString))
-    interactor.putValueAt(colFam,col,tweet.id,tweet.label.getOrElse(99.0).toString)
+    interactor.putValueAt(colFam,col,tweet.id,labelMapper(tweet.label.getOrElse(9999999.0)))
+  }
+
+
+  def labelMapper(label:Double) : String= {
+    val map = Map(0.0 -> "NewYorkFirefighterShooting", 1.0->"ChinaFactoryExplosion", 2.0->"KentuckyAccidentalChildShooting",
+      3.0->"ManhattanBuildingExplosion", 4.0->"NewtonSchoolShooting", 5.0->"HurricaneSandy", 6.0->"HurricaneArthur",
+      7.0->"HurricaneIsaac", 8.0->"TexasFertilizerExplosion")
+    map.getOrElse(label,"CouldNotClassify")
   }
 }
 
