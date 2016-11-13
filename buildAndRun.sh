dc45506670a5ccee96ea4d1d06012f270a578f52
ewmson <cs5604f16_cla@node1.dlrl>
2016-11-13 12:36:55 -0500

    add in driver memory

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -11,6 +11,6 @@ echo building the project
 sbt package
 
 echo running the script on collection "$1"
-spark-submit  --jars stanford-corenlp/jars/stanford-corenlp-3.4.1-models.jar,stanford-corenlp/jars/stanford-corenlp-3.4.1.jar --class isr.project.SparkGrep target/scala-2.10/sparkgrep_2.10-1.0.jar "$1" 9
+spark-submit  --driver-memory 20G --jars stanford-corenlp/jars/stanford-corenlp-3.4.1-models.jar,stanford-corenlp/jars/stanford-corenlp-3.4.1.jar --class isr.project.SparkGrep target/scala-2.10/sparkgrep_2.10-1.0.jar "$1" 9
 #spark-submit  --jars stanford-corenlp/jars/stanford-corenlp-3.4.1-models.jar,stanford-corenlp/jars/stanford-corenlp-3.4.1.jar --class isr.project.SparkGrep target/scala-2.10/sparkgrep_2.10-1.0.jar "local[*]" "$1" "$2" 9
 
