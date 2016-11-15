696ab8df81034df54a72fead20c732469cbf0102
Eric Williamson <ericrw96@vt.edu>
2016-11-15 09:52:15 -0500

    adding back in broadcasting

--- src/main/scala/Word2VecClassifier.scala
+++ src/main/scala/Word2VecClassifier.scala
@@ -28,7 +28,7 @@ def predict(tweets:RDD[Tweet], sc:SparkContext): RDD[Tweet] ={
 
     //Broadcast the variables
     val bcNumberOfClasses = sc.broadcast(_numberOfClasses)
-    //val bcWord2VecModelFilename = sc.broadcast(_word2VecModelFilename)
+    val bcWord2VecModelFilename = sc.broadcast(_word2VecModelFilename)
     val bcLRClassifierModelFilename = sc.broadcast(_lrModelFilename)
 
     def cleanHtml(str: String) = str.replaceAll( """<(?!\/?a(?=>|\s.*>))\/?.*?>""", "")
@@ -36,17 +36,17 @@ def predict(tweets:RDD[Tweet], sc:SparkContext): RDD[Tweet] ={
     def cleanTweetHtml(sample: Tweet) = sample copy (tweetText = cleanHtml(sample.tweetText))
 
     val cleanTestTweets = tweets map cleanTweetHtml
-    val word2vecModel = sc.broadcast(Word2VecModel.load(sc, _word2VecModelFilename))
-    println(s"Model file found:${_word2VecModelFilename}. Loading model.")
+    val word2vecModel = Word2VecModel.load(sc, bcWord2VecModelFilename.value)
+    println(s"Model file found:${bcWord2VecModelFilename.value}. Loading model.")
     println("Finished Training")
-    println(word2vecModel.value.transform("hurricane"))
+    println(word2vecModel.transform("hurricane"))
 
     // Words only
     def cleanWord(str: String) = str.split(" ").map(_.trim.toLowerCase).filter(_.size > 0).map(_.replaceAll("\\W", "")).reduceOption((x, y) => s"$x $y")
 
     def wordOnlySample(sample: Tweet) = sample copy (tweetText = cleanWord(sample.tweetText).getOrElse(""))
 
-    def wordFeatures(words: Iterable[String]): Iterable[Vector] = words.map(w => Try(word2vecModel.value.transform(w))).filter(_.isSuccess).map(x => x.get)
+    def wordFeatures(words: Iterable[String]): Iterable[Vector] = words.map(w => Try(word2vecModel.transform(w))).filter(_.isSuccess).map(x => x.get)
 
     def avgWordFeatures(wordFeatures: Iterable[Vector]): Vector = Vectors.fromBreeze(wordFeatures.map(_.toBreeze).reduceLeft((x, y) => x + y) / wordFeatures.size.toDouble)
 
