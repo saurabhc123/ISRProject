0ef3dd8e432547d67144d6167b3ab55ddc52271f
Eric Williamson <ericrw96@vt.edu>
2016-11-16 22:15:30 -0500

    add caching

--- src/main/scala/DataWriter.scala
+++ src/main/scala/DataWriter.scala
@@ -19,6 +19,7 @@ object DataWriter {
     //val headers = Seq(_col)
     //val rdd: RDD[(String, Seq[String])] = tweets.map({tweet => tweet.id -> Seq(labelMapper(tweet.label.getOrElse(999999.0)))})
     //rdd.toHBase(_tableName, _colFam, headers)
+    tweets.repartition(120)
     tweets.foreachPartition(tweetRDD => {
       val hbaseConf = HBaseConfiguration.create()
       val table = new HTable(hbaseConf,_tableName)
