6067fb2a183062d1e29097b6bb72cf3927dfff6c
Eric Williamson <ericrw96@vt.edu>
2016-11-28 22:55:43 -0500

    fix spark grep to do the normal classification

--- src/main/scala/SparkGrep.scala
+++ src/main/scala/SparkGrep.scala
@@ -2,41 +2,38 @@ package isr.project
 
 import org.apache.spark.mllib.linalg.Word2VecClassifier
 import org.apache.spark.SparkContext
+import org.apache.log4j.{Level, Logger}
 object SparkGrep {
-  def tweetchange(tweet:Tweet): Tweet= {
+  def tweetchange(tweet: Tweet): Tweet = {
     if (tweet.label.get == 0.0) {
       return Tweet(tweet.id, tweet.tweetText, Option(9.0))
     }
     return Tweet(tweet.id, tweet.tweetText, tweet.label)
   }
-	def main(args: Array[String]) {
 
-		if (args.length < 2) {
-			System.err.println("Usage: SparkGrep <collection number to process> <number of classes>")
-			System.exit(1)
-		}
-		val start = System.currentTimeMillis()
-		//Word2VecClassifier.run(args, '|')
-		val sc = new SparkContext()
-		val trainingTweets = DataRetriever.getTrainingTweets(sc)
-			trainingTweets.collect().foreach(println)
-    Word2VecClassifier.train(trainingTweets,sc)
-		println(trainingTweets.map(tweet => tweet.label).filter(_.isDefined).map(e => e.get).distinct().collect().sorted.foreach(println))
-		DataWriter.writeTrainingData(trainingTweets.map(tweet => tweetchange(tweet)))
-		//val cleanTweet = CleanTweet.clean(trainingTweets,sc)
-		//DataWriter.writeTrainingData(cleanTweet)
-    /*val readTweets = DataRetriever.retrieveTweets(args(0),sc)
-    val cleanTweets = CleanTweet.clean(readTweets,sc)
-    val predictedTweets = Word2VecClassifier.predict(cleanTweets,sc)
-		DataWriter.writeTweets(predictedTweets)*/
-		//MultiClassOrchestrator.train(args, '|')
+  def main(args: Array[String]) {
+
+    if (args.length < 3) {
+      System.err.println("Usage: SparkGrep <collection number to process> <number of classes> <blockCount>")
+      System.exit(1)
+    }
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+
+    val start = System.currentTimeMillis()
+    //Word2VecClassifier.run(args, '|')
+    val sc = new SparkContext()
+    val readTweets = DataRetriever.retrieveTweets(args, sc)
+    //val cleanTweets = CleanTweet.clean(readTweets,sc)
+    //val predictedTweets = Word2VecClassifier.predict(cleanTweets,sc)
+    //DataWriter.writeTweets(predictedTweets)
+    //MultiClassOrchestrator.train(args, '|')
     //Orchestrator.train(args)
-		//FpGenerate.generateFrequentPatterns("data/issac.txt", args)
-		//SparkUtilities.countWords(args)
-		//WordVectorGenerator.generateWordVector("data/issac.txt", args)
-		//CleanTweet.clean(args,"data/multi_class_lem")
-		val end = System.currentTimeMillis()
-    println(s"Took ${(end-start)/1000.0} seconds for the whole process.")
-	}
-
+    //FpGenerate.generateFrequentPatterns("data/issac.txt", args)
+    //SparkUtilities.countWords(args)
+    //WordVectorGenerator.generateWordVector("data/issac.txt", args)
+    //CleanTweet.clean(args,"data/multi_class_lem")
+    val end = System.currentTimeMillis()
+    println(s"Took ${(end - start) / 1000.0} seconds for the whole process.")
+  }
 }
