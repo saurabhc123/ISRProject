718fad4f5717c7ced3ae2263f1322e98c9d2b3d0
Eric Williamson <ericrw96@vt.edu>
2016-12-04 15:27:06 -0500

    Dev_manual_retriever_configuration

--- src/main/scala/DataRetriever.scala
+++ src/main/scala/DataRetriever.scala
@@ -18,22 +18,18 @@ object DataRetriever {
   val _lrModelFilename = "data/lrclassifier.model"
   var _cachedRecordCount = 50
   var _tableName: String = "ideal-cs5604f16" /*"ideal-cs5604f16-fake"*/
-  var _colFam : String = "tweet"
-  var _col : String = "cleantext" /*"text"*/
+  var _columnFamily : String = "tweet"
+  var _Column : String = "cleantext" /*"text"*/
   var _word2VecModelFilename = "data/word2vec.model"
 
   def retrieveTweets(args: Array[String], sc: SparkContext): RDD[Tweet] = {
     //implicit val config = HBaseConfig()
 
+    // parse the collection ID from program arguments
     val collectionID = args(0)
     if (args.length >= 2)
       _cachedRecordCount = args(2).toInt
-    val scan = new Scan(Bytes.toBytes(collectionID), Bytes.toBytes(collectionID + '0'))
-    val hbaseConf = HBaseConfiguration.create()
-    val table = new HTable(hbaseConf,_tableName)
-    scan.addColumn(Bytes.toBytes(_colFam), Bytes.toBytes(_col))
-    scan.setCaching(_cachedRecordCount)
-    scan.setBatch(100)
+
 
     val bcWord2VecModelFilename = sc.broadcast(_word2VecModelFilename)
     val bcLRClassifierModelFilename = sc.broadcast(_lrModelFilename)
@@ -46,8 +42,15 @@ object DataRetriever {
     val predictedTweets = Word2VecClassifier.predict(coldTweet, sc, word2vecModel, logisticRegressionModel)
     predictedTweets.count
 
-
-
+    // scan over only the collection
+    val scan = new Scan(Bytes.toBytes(collectionID), Bytes.toBytes(collectionID + '0'))
+    val hbaseConf = HBaseConfiguration.create()
+    val table = new HTable(hbaseConf,_tableName)
+    // add the specific column to scan
+    scan.addColumn(Bytes.toBytes(_columnFamily), Bytes.toBytes(_Column))
+    // add caching to increase speed
+    scan.setCaching(_cachedRecordCount)
+    scan.setBatch(100)
     val resultScanner = table.getScanner(scan)
     println(s"Caching Info:${scan.getCaching} Batch Info: ${scan.getBatch}")
     println("Scanning results now.")
@@ -58,10 +61,10 @@ object DataRetriever {
       try {
         println("Getting next batch of results now.")
         val start = System.currentTimeMillis()
-        val results = resultScanner.next(_cachedRecordCount)
-        //var resultTweets = rowToTweetConverter(resultScanner.next(_cachedRecordCount))
 
-        if (results == null || results.length == 0)
+        val results = resultScanner.next(_cachedRecordCount)
+
+        if (results == null || results.isEmpty)
           continueLoop = false
         else {
           println(s"Result Length:${results.length}")
@@ -74,32 +77,17 @@ object DataRetriever {
           println("*********** Predicting the tweets now. *****************")
           val predictedTweets = Word2VecClassifier.predict(cleanTweets, sc, word2vecModel, logisticRegressionModel)
           println("*********** Persisting the tweets now. *****************")
-          //actualTweets.map(t => println(s"Tweet Text:${t.tweetText} Label:${t.label}"))
 
+          val repartitionedPredictions = predictedTweets.repartition(12)
+          DataWriter.writeTweets(repartitionedPredictions)
 
-          val firstTweet = predictedTweets.take(1)
-          firstTweet.foreach(actualTweets =>
-            println(s"Tweet Text:${actualTweets.tweetText} Label:${actualTweets.label}"))
-          //val i = firstTweet.length
-          //predictedTweets.collect().take(1).foreach(s => s"Tweet Text:${s.tweetText} Label:${s.label}")
           predictedTweets.cache()
           val batchTweetCount = predictedTweets.count()
-          val repartitionedPredictions = predictedTweets.repartition(12)
           println(s"The amount of tweets to be written is $batchTweetCount")
-          DataWriter.writeTweets(repartitionedPredictions)
           val end = System.currentTimeMillis()
           totalRecordCount += batchTweetCount
           println(s"Took ${(end-start)/1000.0} seconds for This Batch.")
           println(s"This batch had $batchTweetCount tweets. We have processed $totalRecordCount tweets overall")
-          continueLoop = false
-          //puts.collect()
-          //val recordCount = puts.count()
-//          println("Wrote to database " + recordCount + " tweets")
-
-          //val actualTweets = predictedTweets.take(1)
-          //actualTweets.map(t => println(s"Tweet Text:${t.tweetText} Label:${t.label}"))
-          //totalRecordCount = predictedTweets.count().toInt + 1
-          //println(s"Tweet Text:${actualTweets} Label:${actualTweets.label}"))
         }
       }
       catch {
@@ -132,7 +120,7 @@ object DataRetriever {
   }
 
   def rowToTweetConverter(result : Result): Tweet ={
-    val cell = result.getColumnLatestCell(Bytes.toBytes(_colFam), Bytes.toBytes(_col))
+    val cell = result.getColumnLatestCell(Bytes.toBytes(_columnFamily), Bytes.toBytes(_Column))
     val key = Bytes.toString(cell.getRowArray, cell.getRowOffset, cell.getRowLength)
     val words = Bytes.toString(cell.getValueArray, cell.getValueOffset, cell.getValueLength)
     Tweet(key,words)
