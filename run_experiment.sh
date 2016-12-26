ea0f7e5b81543cc6a666cd0795a78391e14da941
ewmson <cs5604f16_cla@node1.dlrl>
2016-12-26 18:03:22 -0500

    add in the exporting so that the yarn cluster can be run

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+export HADOOP_CONF_DIR=/etc/hadoop/conf
 sbt package
-spark-submit --driver-memory 20G --class ExperimentRunner target/scala-2.10/sparkgrep_2.10-1.0.jar
+spark-submit --master yarn --driver-memory 20G --class ExperimentRunner target/scala-2.10/sparkgrep_2.10-1.0.jar
 
