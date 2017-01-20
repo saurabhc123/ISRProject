c21502addf573ab971bfab1e73eb3762a7e00d0e
ewmson <saurabc@yahoo.com>
2017-01-19 20:16:15 -0500

    add in latest cluster changes

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export HADOOP_CONF_DIR=/etc/hadoop/conf
 sbt package
-spark-submit --master yarn --num-executors 8 --executor-cores 4 --driver-memory 4G --executor-memory 2G --class ExperimentRunner target/scala-2.10/sparkgrep_2.10-1.0.jar
+spark-submit --master yarn --num-executors 8 --executor-cores 4 --driver-memory 4G --executor-memory 4G --class ExperimentRunner target/scala-2.10/sparkgrep_2.10-1.0.jar
 
